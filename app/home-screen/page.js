67c16580f66c52694f7b073c885be07423e89266
Quan Huynh <152340025+quanhuynh11@users.noreply.github.com>
2025-02-05 18:25:48 -0700

    sessionStorage

--- app/home-screen/page.js
+++ app/home-screen/page.js
@@ -1,7 +1,19 @@
+"use client"
+
+import { useState } from "react";
+
 export default function HomePage() {
+
+    const [user, setUser] = useState(sessionStorage.getItem("user") || null);
+    
+    if(!user) {
+        window.location.href = "/";
+    }
+
     return (
         <section>
             <h1>Home</h1>
+            <p>{user}</p>
         </section>
     )
 }
